3de09f0c5510f74a437583794669ce5811d0d74f
JamshidDev <raximovj662@gmail.com>
2025-08-20 12:36:01 +0500

    cleared comments

--- server.js
+++ server.js
@@ -28,57 +28,6 @@ bot.use(clientComposer)
 
 
 
-// const i18n = new I18n({
-//     defaultLocale: "uz",
-//     useSession: true,
-//     directory: "locale",
-//     globalTranslationContext(ctx) {
-//         return { first_name: ctx.from?.first_name ?? "" };
-//     },
-// })
-// bot.use(session({
-//     type: "multi",
-//     session_db: {
-//         initial: () => {
-//             return {
-//                 condidate: {
-//                     fullname: null,
-//                     birthday: null,
-//                     picture: null,
-//                     pasport:null,
-//                     live_adress: null,
-//                     birth_adress: null,
-//                     phone: null,
-//                     education: null,
-//                     marital_status: null,
-//                 },
-//                 children_list: [],
-//                 husband_woman: {
-//                     fullname: null,
-//                     birthday: null,
-//                     picture: null,
-//                     pasport: null,
-//                 },
-//                 selected_check:null,
-//             }
-//         },
-//         storage: new MemorySessionStorage(),
-//         getSessionKey: (ctx) => ctx.chat?.id.toString(),
-//     },
-//     conversation: {},
-//     __language_code: {},
-// }))
-// bot.use(i18n)
-//
-// bot.use(conversations())
-
-
-
-
-
-
-
-
 
 
 
@@ -93,33 +42,5 @@ bot.catch((err) => {
     console.error(`Xato update ${ctx.update.update_id} da:`, err.error);
     ctx.reply("❌ Botda xato bo'ldi").catch(() => {})
 });
-
-
-
-
-
-
-// app.use(`/${TOKEN}`, webhookCallback(bot, "express"))
-
-// app.use((err, req, res, next) => {
-//     console.error("Express xatosi:", err);
-//     if (!res.headersSent) {
-//         res.status(500).json({ error: "Internal Server Error" });
-//     }
-// });
-
-// app.listen(PORT, async () => {
-//     console.log(`🚀 Server http://localhost:${PORT} da ishlayapti`)
-//
-//     const webhookUrl = `${BASE_URL}/${TOKEN}`
-//
-//     bot.api.setWebhook(webhookUrl, {
-//         allowed_updates:["my_chat_member", "chat_member", "message", "callback_query", "inline_query", "chat_join_request"]
-//     }).then((res)=>{
-//         console.log(`Webhook bot set to ${webhookUrl}`);
-//     }).catch((error)=>{
-//         console.log(error)
-//     });
-// })
 
 bot.start()
